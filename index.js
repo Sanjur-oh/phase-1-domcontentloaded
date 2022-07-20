0d61c7d2269fbefa7d3f9f2056951555ba832273
Michael Franco <franco.luis.michael@gmail.com>
2022-07-20 15:50:03 -0500

    commit tree 7a6b75c88d575fd07cbe3a1a3801ca523f29e208

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded",() => {
+    let d = document.getElementById('text');
+    d.textContent = 'This is really cool!'
+    // console.log("innerText")
+    });
+
+    
+// document.addEventListener("DOMContentLoaded",() => {
+//     let innerText = document.querySelector('#text');
+//     innerText.textContent = 'This is really cool!'
+//     // console.log("innerText")
+//     });
